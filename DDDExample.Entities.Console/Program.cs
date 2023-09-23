5cee8582e9f50617ca3f3ec4d334149e48003ede
Arezo Bagheri <ArezoBagheri@outlook.com>
2023-09-23 22:26:11 +0330

    Adding Validations

---
+++ DDDExample.Entities.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using DDDExample.Entities.Validations;
+
+namespace DDDExample.Entities.TestConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FlightBooking flightBooking = new FlightBooking
+                                  (Guid.NewGuid(), DateTime.Now.AddDays(1), Guid.NewGuid());
+
+            flightBooking.Reschedule(DateTime.Now.AddDays(3));
+            Console.WriteLine("Done");
+            Console.ReadLine();
+
+            flightBooking.Confirm();
+            flightBooking.Reschedule(DateTime.Now.AddDays(3));
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ DDDExample.Entities/Validations/FlightBooking.cs
@@ -0,0 +1,37 @@
+﻿namespace DDDExample.Entities.Validations
+{
+    public class FlightBooking
+    {
+        private bool confirmed = false;
+
+        public Guid Id { get; private set; }
+        public DateTime DepartureDate { get; private set; }
+        public Guid CustomerId { get; private set; }
+
+        public FlightBooking(Guid id, DateTime departureDate, Guid customerId)
+        {
+            if (id == null)
+                throw new Exception("Id Missing");
+
+            if (departureDate == null)
+                throw new Exception("Departure Date Missing");
+
+            if (customerId == null)
+                throw new Exception("Customer Id Missing");
+        }
+
+        public void Reschedule(DateTime newDeparture)
+        {
+            if (confirmed)
+                throw new Exception("Could not change Departure Date confirmation ");
+
+            DepartureDate = newDeparture;
+        }
+
+        public void Confirm()
+        {
+            confirmed = true;
+        }
+
+    }
+}
